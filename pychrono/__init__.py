e24104036c1ba64709a5c2797b0eda444094cc55
Sébastien <sebastien.works@outlook.com>
2024-10-24 12:49:25 +0000

    Last small update, promise! 1.0.6

--- pychrono/__init__.py
+++ pychrono/__init__.py
@@ -3,7 +3,7 @@ from .decorators import asynchronous, cache, schedule, recurring, repeat, retry,
 
 from .functions import delay, current, local, local_raw, countdown
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 __all__ = [
 	"delay",
 	"current",
